dfc25c3a8d0a73cec3cb3d4b39ec4175f02ebdf7
Vedi5246 <148309485+Vedi5246@users.noreply.github.com>
2024-04-17 09:48:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3); //we are using nodejs not browser
+
+let name = "Naveed"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => range: 2 to power 53
+// bigint => if number is too large e.g in trading, reddit,fb 
+// string => ""
+// boolean => true/false
+// null => standalone value , it is an object
+// undefined
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
+
+
