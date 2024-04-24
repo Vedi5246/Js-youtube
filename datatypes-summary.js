35bad2cc5a4de2c79273d6982839f8e427a5cd5b
Vedi5246 <148309485+Vedi5246@users.noreply.github.com>
2024-04-24 11:32:27 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 Types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+let name = "Naveed"
+const score = 100
+const scoreValue = 100
+
+const isLoggedIn = false
+const outideTemp = null
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(Id === anotherId);
+
+// const bigNumber = 774325623743637263847n //this is BigInt
+
+// Reference (Non-Primitive)
+
+// Arrays, Objects, Functions
+
+const heroes = ["Naveed", "Danyal", "Aryan"];
+
+let myObj = {
+    mame: "Naveed",
+    age: 24,
+    gender: "male"
+
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof outideTemp);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
